c42460fe745fda70f51a4a9ff2114b432e98d757
KimDaegyu <ddk3207@naver.com>
2025-08-06 00:32:11 +0900

    chore: 도커, env 파일 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PromiseModule } from './promise/promise.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PromiseModule],
+  imports: [
+    PromiseModule,
+    ConfigModule.forRoot({ cache: true, isGlobal: true }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  const configService = app.get(ConfigService);
   // CORS 설정
   app.enableCors({
     origin: true,
     credentials: true,
   });
 
-  const port = process.env.PORT ?? 3000;
+  const port = configService.get<number>('PORT') ?? 3000;
   await app.listen(port);
   console.log(`🚀 Application is running on: http://localhost:${port}`);
   console.log(`🔌 Socket.IO server is ready for connections`);
